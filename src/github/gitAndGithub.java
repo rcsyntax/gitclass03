87eaca25eb0cd567c17926ae1cd007f41f3ce77b
rcsyntax <rc.syntax@gmail.com>
2023-07-31 00:44:07 -0400

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -4,7 +4,8 @@ public class gitAndGithub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Today we learn github");
+		System.out.println("Update");
 	}
 
 }
